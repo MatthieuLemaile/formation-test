5cfad2b6b01db161f462aff0108e400d4861e64e
Matthieu Lemaile <mlemaile@oxyl.fr>
2023-12-12 10:48:01 +0100

    Add basic test case

---
+++ src/main/java/service/Compute.java
@@ -0,0 +1,24 @@
+package service;
+
+public class Compute {
+
+    public int add2Numbers(int a, int b) {
+        return a + b;
+    }
+
+    public int substract2Numbers(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply2Numbers(int a, int b) {
+        return a * b;
+    }
+
+    public int divide2Numbers(int a, int b) {
+        return a / b;
+    }
+
+    public boolean isGreaterThan(int a, int b) {
+        return a > b;
+    }
+}
---
+++ src/test/java/service/ComputeTest.java
@@ -0,0 +1,24 @@
+package service;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class ComputeTest {
+
+    private final Compute compute = new Compute();
+
+    @Test
+    void when_add_2_and_3_should_give_5() {
+        Assertions.assertEquals(5, compute.add2Numbers(2,3));
+    }
+
+    @Test
+    void when_substract_2_from_3_should_give_1() {
+        Assertions.fail();
+    }
+
+    //Ecrire un test qui vérifie le comportement de multiply2Numbers(2,3)
+    //Ecrire un test qui vérifie le comportement de isGreaterThan(19,7)
+    //Ecrire un test qui vérifie le comportement de isGreaterThan(19,19)
+    //Ecrire un test qui vérifie le comportement de divide2Numbers(3,0)
+}
